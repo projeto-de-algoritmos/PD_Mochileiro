b745a3678fc1f759b09c9b221a533c5b4679037b
Leonardo Barreiros <leossb36@gmail.com>
2021-05-03 00:23:34 -0300

    Adicionando interface

--- src/interface.py
+++ src/interface.py
@@ -1,4 +1,5 @@
 from enum import Enum
+from modules.pygame_textinput import TextInput
 import pygame
 
 class Level(Enum):
@@ -7,15 +8,15 @@ class Level(Enum):
     HARD = 3
 
 class Color(Enum):
-    BLACK = (20,20,20)
-    WHITE = (0, 0, 0)
+    BLACK = (0,0,0)
+    WHITE = (255, 255, 255)
 
 class Display:
     def __init__(self):
         self.height = 500
         self.width = 500
-        self.background = None
         self.screen = None
+        self.clock = None
 
     def initDisplay(self):
         pygame.init()
@@ -23,15 +24,17 @@ class Display:
         windowSize = (self.height, self.width)
         self.screen = pygame.display.set_mode(windowSize)
         pygame.display.set_caption("O Mochileiro")
-        clock = pygame.time.Clock()
+        self.clock = pygame.time.Clock()
+        self.screen.fill(Color.BLACK.value)
 
-        self.background = pygame.Surface.fill(self.screen, Color.BLACK.value)
-
-    def drawElements(self, elements):
+    def drawElements(self, elements, posX, posY):
         pygame.font.init()
-        
 
         text = str(elements)
         font = pygame.font.SysFont('Comic Sans MS', 30)
         text = font.render(text, True, Color.WHITE.value)
-        self.screen.blit(text, (0, 0))
+        self.screen.blit(text, (posX, posY))
+
+    def writeOnDisplay(self, textInput: TextInput(), events):
+        textInput.update(events)
+        self.screen.blit(textInput.get_surface(), (0, 120))
--- src/main.py
+++ src/main.py
@@ -1,13 +1,16 @@
 from item import Item
 from knapsack import knapsack, initMatrix
+from modules.pygame_textinput import TextInput
 from interface import *
 import random as rnd
 import pygame
+import time
+
 
 
 def backgroundKnapsack():
     items = []
-    
+
     limitWeight = rnd.randint(10, 30)
     sizeArray = rnd.randint(5, 10)
 
@@ -33,17 +36,44 @@ def backgroundKnapsack():
 
     result = knapsack(weightArray, valueArray, limitWeight, sizeArray, matrix)
 
-    return weightArray, valueArray, result
+    return weightArray, valueArray, limitWeight, sizeArray, result
 
 
 if __name__ == "__main__":
 
     display = Display()
-    running = True
-    weightArray, valueArray, result = backgroundKnapsack()
+    display.initDisplay()
+    textInput = TextInput()
+    gameExit = True
+    retry = 3
+    weightArray, valueArray, limitWeight, sizeArray, result = backgroundKnapsack()
 
-    while running:
-        display.initDisplay()
-        display.drawElements(weightArray)
+    while gameExit:
+        events = pygame.event.get()
+        for event in events:
+            if event.type == pygame.QUIT:
+                gameExit = False
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_RETURN:
+                    response = int(textInput.get_text())
+                    if (response != result):
+                        display.drawElements("Resposta errada, tente novamente", 0, 140)
+                        retry = retry - 1
+                    else:
+                        display.drawElements("Parabéns você acertou!", 0, 140)
 
-        pygame.display.flip()
+        if retry < 1:
+            display.drawElements("Você perdeu =(", 0, 160)
+            gameExit = False
+        display.drawElements("Limite da mochila: " + str(limitWeight), 0, 0)
+        display.drawElements("Quantidade de itens: " + str(sizeArray), 0, 20)
+        display.drawElements("Pesos: " + str(weightArray), 0, 60)
+        display.drawElements("Valores: " + str(valueArray), 0, 80)
+        display.drawElements("Digite o valor de resposta: ", 0, 100)
+        display.writeOnDisplay(textInput, events)
+
+        pygame.display.update()
+        display.clock.tick(30)
+
+    time.sleep(1)
+    pygame.quit()
